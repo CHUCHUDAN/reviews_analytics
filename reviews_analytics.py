a7d6f7f9dd8ef563d17b1e9404cb1b2e9d2dcf29
daniel <daniel870217@gmail.com>
2022-08-06 14:53:18 +0800

    caculate

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -1,10 +1,13 @@
 data=[]
-count = 0
+sum = 0
 with open("reviews.txt","r") as f:
 	for line in f:
 		data.append(line)
-		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+	for oo in data:
+		n = len(oo)
+		sum += n
+
+print("每筆平均資料長度是", sum/len(data))
+	
 
 	
