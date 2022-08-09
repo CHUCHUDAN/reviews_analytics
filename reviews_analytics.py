eb97dab41ac4f83e5bddba10eb0b740d29ee1759
CHUCHUDAN <110530032+CHUCHUDAN@users.noreply.github.com>
2022-08-09 12:55:30 +0800

    Add files via upload

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -1,20 +1,66 @@
 data=[]
-sum = 0
+count = 0
 with open("reviews.txt","r") as f:
 	for line in f:
 		data.append(line)
-	for oo in data:
-		n = len(oo)
-		sum += n
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+# print("檔案讀取完了,總共有",len(data),"筆資料")
+# print(data[0])
 
-print("每筆平均資料長度是", sum/len(data))
-	
-new = []
+wc = {}
 for d in data:
-	if len(d) < 100:
-		new.append(d)
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+			
+		else:
+			wc[word] = 1
 
-print("一共有", len(new), "筆小於100的資料")
+for word in wc:
+	if wc[word] >= 1000000:
+		print(word,wc[word])
+
+print(len(wc))
+print(wc["Daniel"])
+
+while True:
+	word = input("想查詢甚麼字?: ")
+	if word == "q":
+		break
+	if word in wc:
+		print(word,"出現過",wc[word],"次")
+	else:
+		print("資料中沒這個字")
+
+print("感謝您使用本查詢系統")
+
+
+
+
+
+
+# sum_len = 0
+# for d in data:
+# 	n = len(d)
+# 	sum_len += n
+		
+# print("每筆平均資料長度是", sum_len/len(data))
+
+
 	
-print(new[0])
-print(new[1])
+# new = []
+# for d in data:
+# 	if len(d) < 100:
+# 		new.append(d)
+
+# print("一共有", len(new), "筆小於100的資料")
+
+# good = []
+# for d in data:
+# 	if "good" in d:
+# 		good.append(d)
+
+# print(len(good))
